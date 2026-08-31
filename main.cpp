d63c8fbb5e6bf280e8079c34275a2132fc9856aa
Smoothoperator25 <kartikbhand8@gmail.com>
2026-08-31 23:03:34 +0530

    Formating dock

--- main.cpp
+++ main.cpp
@@ -9,21 +9,32 @@ class Supplier
     vector<Product> products;
 
 public:
-    Supplier(){
+    Supplier()
+    {
         SupplierID = 0;
         SupplierName = "";
     }
 
-    Supplier(){
-        
+    Supplier(int id, string name)
+    {
+        SupplierID = id;
+        SupplierName = name;
     }
+
     void supplyProduct();
     void updateDetails();
 };
 
 void Supplier ::supplyProduct()
 {
+    string proName;
 
+    cout << "Enter the product name : ";
+    getline(cin, proName);
+
+    products.push_back(proName);
+
+    cout << "Successfull\n\n";
 }
 
 int main()
